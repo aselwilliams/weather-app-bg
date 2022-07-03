f7cea93bc6038eeef704bc0698209c89bd1f510c
aselwilliams <aselwilliams22@gmail.com>
2022-07-03 10:58:45 -0400

    get date func

--- src/App.js
+++ src/App.js
@@ -7,7 +7,14 @@ const api={
 function App() {
 
   const dateBuilder=(d)=>{
-  
+  let months=['January','Febuary','March', 'April','May','June','July','August','September','October','November','December'];
+  let days=['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+  let day=days[d.getDay()];
+  let date=d.getDate();
+  let month=months[d.getMonth()];
+  let year=d.getFullYear();
+
+  return `${day} ${date} ${month} ${year}`
   }
   return (
     <div className="app">
