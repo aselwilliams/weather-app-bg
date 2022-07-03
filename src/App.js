d14d8739b5c95b4fa1a7e074e72abe81b1b417f6
aselwilliams <aselwilliams22@gmail.com>
2022-07-03 10:14:42 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+const api={
+  base:'https://api.openweathermap.org/data/2.5',
+  key:'745b34e39ffe426f3d05796a851f853e'
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
     </div>
   );
 }
